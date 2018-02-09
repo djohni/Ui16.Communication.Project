00f7d462bd97e85eb2254cc26a0aaa451c249960
Stefán János <itstefanjanos@gmail.com>
2018-02-09 11:10:55 +0100

    Ui16 Communication Protocol test.

---
+++ UiTest/UiTest/Form1.Designer.cs
@@ -0,0 +1,107 @@
+﻿namespace UiTest
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.ConnectButton = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.vScrollBar1 = new System.Windows.Forms.VScrollBar();
+            this.tableLayoutPanel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // ConnectButton
+            // 
+            this.ConnectButton.Location = new System.Drawing.Point(3, 3);
+            this.ConnectButton.Name = "ConnectButton";
+            this.ConnectButton.Size = new System.Drawing.Size(75, 23);
+            this.ConnectButton.TabIndex = 2;
+            this.ConnectButton.Text = "Connect";
+            this.ConnectButton.UseVisualStyleBackColor = true;
+            this.ConnectButton.Click += new System.EventHandler(this.ConnectButton_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.textBox1.Location = new System.Drawing.Point(3, 35);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(653, 544);
+            this.textBox1.TabIndex = 3;
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.ColumnCount = 2;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 50F));
+            this.tableLayoutPanel1.Controls.Add(this.textBox1, 0, 1);
+            this.tableLayoutPanel1.Controls.Add(this.ConnectButton, 0, 0);
+            this.tableLayoutPanel1.Controls.Add(this.vScrollBar1, 1, 0);
+            this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 0);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 2;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 32F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle());
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(709, 582);
+            this.tableLayoutPanel1.TabIndex = 4;
+            // 
+            // vScrollBar1
+            // 
+            this.vScrollBar1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.vScrollBar1.Location = new System.Drawing.Point(659, 0);
+            this.vScrollBar1.Maximum = 1009;
+            this.vScrollBar1.Name = "vScrollBar1";
+            this.tableLayoutPanel1.SetRowSpan(this.vScrollBar1, 2);
+            this.vScrollBar1.Size = new System.Drawing.Size(50, 582);
+            this.vScrollBar1.TabIndex = 4;
+            this.vScrollBar1.Value = 1000;
+            this.vScrollBar1.ValueChanged += new System.EventHandler(this.vScrollBar1_ValueChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(709, 582);
+            this.Controls.Add(this.tableLayoutPanel1);
+            this.Name = "Form1";
+            this.Text = "UiTest";
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.tableLayoutPanel1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button ConnectButton;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.VScrollBar vScrollBar1;
+    }
+}
+
---
+++ UiTest/UiTest/Form1.cs
@@ -0,0 +1,170 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using WebSocketSharp;
+
+namespace UiTest
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            
+        }
+        private string outData;
+        private bool connected = false;
+        private int oldValue = 0;
+        private int newValue = 0;
+        private WebSocket client;
+
+        private void ConnectButton_Click(object sender, EventArgs e)
+        {
+            if (!connected) //Connect
+            {
+                string content;
+                using (var wc = new System.Net.WebClient())
+                    content = wc.DownloadString($"http://192.168.1.104/socket.io/?t={(DateTime.Now - new DateTime(1970, 1, 1, 1, 0, 0)).Ticks / 10000}");
+                var contentSplit = content.Split(':');
+                string uri = $"ws://192.168.1.104/socket.io/{contentSplit[3]}/{contentSplit[0]}";
+                
+                string[][] inputStrings;
+                client = new WebSocket(uri);
+                client.OnOpen += (ss, ee) =>
+                {
+                    textBox1.AppendText($"OnOpen Event Listener{Environment.NewLine}");
+                    StartAliveTimer();
+                };
+                client.OnError += (ss, ee) =>
+                    Debugger.Break();
+                client.OnMessage += (ss, ee) =>
+                {
+                    if (InvokeRequired)
+                    {
+                        Invoke(new Action(() => { textBox1.AppendText($"IN: {ee.Data}{Environment.NewLine}"); }));
+                        if (ee.Data == "1::")
+                        {
+                            outData =
+                                $"3:::USERTIME^{(DateTime.Now - new DateTime(1970, 1, 1, 1, 0, 0)).Ticks / 10000}^-60";
+                            client.Send(outData);
+                            Invoke(new Action(() => textBox1.AppendText($"OUT: {outData}{Environment.NewLine}")));
+                        }
+                        else if (ee.Data == "2::")
+                        {
+                            client.Send("2::");
+                            Invoke(new Action(() => textBox1.AppendText($"OUT: 2::{Environment.NewLine}")));
+                        }
+                        else if (ee.Data.StartsWith("3:::"))
+                        {
+                            inputStrings = ee.Data.Substring(4).Split(new []{'\n', '='}, StringSplitOptions.RemoveEmptyEntries).Select(p => p.Split('^')).ToArray();
+                            if (inputStrings.First()[0] == "UPDATE_PLAYLIST")
+                            {
+                                string currentPlaylist = "";
+                                for (int i = 1; i < inputStrings.Length; i++)
+                                {
+                                    if (inputStrings[i].Length > 1 && inputStrings[i][1] == "var.currentPlaylist")
+                                    {
+                                        currentPlaylist = inputStrings[i][2];
+                                        break;
+                                    }
+                                }
+                                client.Send("3:::MEDIA_GET_PLISTS");
+                                Invoke(new Action(() => textBox1.AppendText($"OUT: 3:::MEDIA_GET_PLISTS{Environment.NewLine}")));
+                                client.Send($"3:::MEDIA_GET_PLIST_TRACKS^{currentPlaylist}");
+                                Invoke(new Action(() => textBox1.AppendText($"OUT: 3:::MEDIA_GET_PLIST_TRACKS^{currentPlaylist}{Environment.NewLine}")));
+                            }
+                            for (int i = 0; i < inputStrings.Length; i++)
+                            {
+                                if (inputStrings[i].Length > 1 && inputStrings[i][1] == "i.0.mix")
+                                {
+                                    oldValue = 1000 - (int)(double.Parse(inputStrings[i][2]) * 1000);
+                                    newValue = 1000 - (int)(double.Parse(inputStrings[i][2]) * 1000);
+                                    Invoke(new Action(() =>
+                                    {
+                                        vScrollBar1.Value = newValue;
+                                    }));
+                                    break;
+                                }
+                            }
+                            for (int i = 0; i < 3; i++)
+                            {
+                                if (oldValue != newValue)
+                                {
+                                    outData =
+                                        $"3:::SETD^i.0.mix^{1.0 - newValue / 1000.0}";
+                                    client.Send(outData);
+                                    Invoke(new Action(() => textBox1.AppendText($"OUT: {outData}{Environment.NewLine}")));
+                                    oldValue = newValue;
+                                }
+                                else
+                                {
+                                    break;
+                                }
+                            }
+                            
+                        }
+                    }
+                };
+                    
+                client.OnClose += (ss, ee) =>
+                {
+                    tokenSource.Cancel();
+                    if (InvokeRequired)
+                    {
+                        Invoke(new Action(() => { textBox1.AppendText($"OnClose Event Listener{Environment.NewLine}"); }));
+                    }
+                };
+                client.Connect();
+                textBox1.AppendText($"Connected to {uri}{Environment.NewLine}");
+                ConnectButton.Text = "Disconnect";
+                connected = true;
+            }
+            else //Disconnect
+            {
+                client.Close();
+                ConnectButton.Text = "Connect";
+                connected = false;
+            }
+        }
+
+        private Stopwatch sw;
+        private CancellationTokenSource tokenSource;
+        private CancellationToken cancellationToken;
+        private async Task StartAliveTimer()
+        {
+            tokenSource = new CancellationTokenSource();
+            cancellationToken = tokenSource.Token;
+            await Task.Factory.StartNew(() =>
+            {
+                if (sw == null) sw = new Stopwatch();
+                else sw.Reset();
+                sw.Start();
+                double elapsedMilliseconds = sw.Elapsed.TotalMilliseconds;
+                while (true)
+                {
+                    if(tokenSource.IsCancellationRequested)
+                        break;
+                    if (sw.Elapsed.TotalMilliseconds - elapsedMilliseconds >= 1000)
+                    {
+                        elapsedMilliseconds = sw.Elapsed.TotalMilliseconds;
+                        if (!tokenSource.IsCancellationRequested)
+                            client.SendAsync("3:::ALIVE", b => Invoke(new Action(() => { textBox1.AppendText($"OUT ({b}): 3:::ALIVE{Environment.NewLine}"); })));
+                    }
+                }
+            }, cancellationToken);
+        }
+
+        private void vScrollBar1_ValueChanged(object sender, EventArgs e)
+        {
+            newValue = vScrollBar1.Value;
+        }
+    }
+}
